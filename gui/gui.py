9cbe9c2674de86cc0127cf62aee864171f3e119c
FrancescoTerrosi <francesco.terrosi1@gmail.com>
2025-05-05 04:34:47 +0200

    GUI update

--- gui/gui.py
+++ gui/gui.py
@@ -1,48 +1,84 @@
 import sys
-from PySide6.QtWidgets import (QApplication, QMainWindow, QDialog,
-                                QTextEdit, QPushButton,
-                                QVBoxLayout, QTableWidget,
-                                QLabel,  QTableWidgetItem)
+from datetime import datetime
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QTextBrowser, QFrame, QTextEdit, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QTreeView, QTreeWidgetItem, QTreeWidget
 from PySide6.QtCore import Qt, Slot
-from PySide6.QtGui import QColor
+from PySide6.QtGui import QColor, QPainter, QStandardItemModel, QStandardItem
 
-class Form(QMainWindow):
 
-    def greetings(self):
-        output = self.agent.run(self.edit.toPlainText())
+class MainWindow(QMainWindow):
+
+    def sendMessage(self):
+        output = self.agent.run(self.edit.text())
+        #output = "Hello World!"
 
         self.message_index += 1
+
+        #DEV
         history = self.agent.getHistory()
-        label = QLabel(history[-2])
-        label.setWordWrap(True)
-        label.setAlignment(Qt.AlignLeft)
-        self.mainLayout.insertWidget(self.message_index, label)
+        #self.history.append("MESSAGGIO")
+        #self.history.append(output)
 
+        self.subLayout.itemAt(0).widget().setAlignment(Qt.AlignLeft)
+        self.subLayout.itemAt(0).widget().append("[IO] - " + datetime.today().strftime("%y %m %d %H:%M:%S") + "\n" + history[-2] + "\n\r")
         self.message_index += 1
-        label = QLabel(history[-1])
-        label.setWordWrap(True)
-        label.setAlignment(Qt.AlignRight)
-        self.mainLayout.insertWidget(self.message_index, label)
+        self.subLayout.itemAt(0).widget().setAlignment(Qt.AlignRight)
+        self.subLayout.itemAt(0).widget().append("[AGENT] - " + datetime.today().strftime("%y %m %d %H:%M:%S") + "\n" + history[-1] + "\n\r")
 
-    def __init__(self, parent=None, agent=None):
-        super(Form, self).__init__(parent)
+    def __init__(self, agent=None):
+        super().__init__()
         self.agent = agent
         self.message_index = -1
 
-        self.setMinimumWidth(800)
-        self.setMinimumHeight(600)
+        #DEV
+        self.history = []
 
-        self.edit = QTextEdit("Write my name here..")
-        self.button = QPushButton("Show Greetings")
-        self.button.clicked.connect(self.greetings)
+        self.top = QTextBrowser()
 
-        self.mainLayout = QVBoxLayout()
+        self.top.setFrameShape(QFrame.Shape.Box)
+        self.top.setFixedWidth(600)
 
-        subLayout = QVBoxLayout()
-        subLayout.addWidget(self.edit)
-        subLayout.addWidget(self.button)
+        self.edit = QLineEdit()
+        self.edit.setFixedHeight(200)
+        self.edit.setFixedWidth(600)
 
-        subLayout.setContentsMargins(100, 0, 100, 0)
+        self.button = QPushButton("Press Me!")
+        self.edit.setFixedHeight(100)
+        self.edit.setFixedWidth(600)
+        self.edit.setTextMargins(10, 0, 0, 60)
+        self.button.clicked.connect(self.sendMessage)
 
-        self.mainLayout.addLayout(subLayout)
+
+        self.mainWidget = QDialog()
+        self.mainLayout = QHBoxLayout(self.mainWidget)
+
+        self.subLayout = QVBoxLayout()
+        self.subLayout.addWidget(self.top)
+
+        self.subLayout.addWidget(self.edit)
+        self.subLayout.addWidget(self.button)
+        self.subLayout.setAlignment(Qt.AlignHCenter)
+
+
+        #self.left = QVBoxLayout()
+        #behaviourTree = QTreeWidget()
+        #behaviourTree.setColumnCount(1)
+        #item1 = QTreeWidgetItem("row %0 column %1")
+        #behaviourTree. item1)
+        #item2 = QTreeWidgetItem("row %0 column %1")
+        #behaviourTree.setItem(1, item2)
+        #item3 = QTreeWidgetItem("row %0 column %1")
+        #behaviourTree.setItem(2, item3)
+        #item4 = QTreeWidgetItem("row %0 column %1")
+        #behaviourTree.setItem(3, item4)
+
+        #treeView = QTreeView()
+        #treeView.setModel(behaviourTree)
+
+        #self.left.addWidget(treeView)
+        #self.left.setAlignment(Qt.AlignTop)
+        #self.mainLayout.addLayout(self.left)
+
+        self.mainLayout.addLayout(self.subLayout)
+
+        self.setCentralWidget(self.mainWidget)
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from gui.gui import Form
+from gui.gui import MainWindow
 from src.aichat import ChatBot
 import sys
 from PySide6.QtWidgets import QApplication
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     # Create the Qt Application
     app = QApplication(sys.argv)
     # Create and show the form
-    form = Form(agent = ChatBot())
-    form.show()
+    mainWindow = MainWindow(agent = ChatBot())
+    mainWindow.resize(800, 600)
+    mainWindow.show()
     # Run the main Qt loop
     sys.exit(app.exec())
 
